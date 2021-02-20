bfc2fa0e14044975dc07ccd5169e33fbdf8e855a
Jacopo Marchi <79257470+Jacopo-Marchi@users.noreply.github.com>
2021-02-20 13:27:14 +0100

    Add files via upload

--- figs_paper_coarse_gr_clean/python_code/plots/fig3.py
+++ figs_paper_coarse_gr_clean/python_code/plots/fig3.py
@@ -1,9 +1,19 @@
-####
-# Figure 3
-# needs:
-# - data/1d.npz produced by intra_host_neut.py
-# - data/2d.npz produced by run2d.py
-####
+
+"""
+  *  Copyright (C) 2021 Jacopo Marchi
+  * 
+  *  This program is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  * 
+"""
 import matplotlib
 matplotlib.use('Agg')
 import os
